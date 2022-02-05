8fe68e678f4c27a5c52856805ec47cb0e6f097f4
Dhanakumaresh <d.subramani@reassist.de>
2022-02-05 22:18:39 +0100

    feat:added end point for the customer

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const app = express();
 const port = 3000;
+let a;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.post('/client', (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
-
